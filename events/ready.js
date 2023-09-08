8acb3162af04b573bf5eaf3cab95edfcd948a461
Michael Connolly <mpc63@uakron.edu>
2023-09-08 16:19:55 -0400

    Add MongoDB connection

--- events/ready.js
+++ events/ready.js
@@ -1,9 +1,21 @@
 const { Events } = require('discord.js');
+const mongoose = require('mongoose');
+
+const mongoDB = process.env.MONGODB;
 
 module.exports = {
   name: Events.ClientReady,
   once: true,
   async execute(client) {
+    await mongoose.connect(mongoDB, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+
+    if (mongoose.connect) {
+      console.log('Connected to MongoDB');
+    }
+
     console.log(`Logged in as ${client.user.tag}`);
     console.log(`Servers (${client.guilds.cache.size}):`);
     console.log(
