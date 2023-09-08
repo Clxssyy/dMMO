05356febcd9959a4ab4d2d9e36cfa423662c61ee
Michael Connolly <mpc63@uakron.edu>
2023-09-07 21:42:04 -0400

    Add editing functionality

---
+++ events/messageUpdate.js
@@ -0,0 +1,26 @@
+const { Events } = require('discord.js');
+
+module.exports = {
+  name: Events.MessageUpdate,
+  async execute(message) {
+    if (message.author.bot) return;
+
+    const user = message.author.id;
+    var level = 0;
+    var newLevel = level;
+
+    if (level == 0) {
+      newLevel = 1;
+    } else {
+      newLevel += 1 / (Math.floor(level) * 10);
+    }
+
+    if (newLevel > level) {
+      message.channel.send(
+        `${
+          message.author.username
+        } has leveled up Editing to level ${Math.floor(newLevel)}!`
+      );
+    }
+  },
+};
