d62365ee3e67e74e87233f6a68c1130970b98722
Michael Connolly <mpc63@uakron.edu>
2024-07-13 15:16:33 -0400

    Fix stats command

--- commands/info/stats.js
+++ commands/info/stats.js
@@ -17,23 +17,54 @@ module.exports = {
         ephemeral: true,
       });
 
-    let userStats = await serverSchema.findOne({
-      serverID: interaction.guild.id,
-      userID: String(user.id),
-    });
+    const server = interaction.guild;
+    let serverData = await serverSchema.findOne({ serverID: server.id });
+
+    if (!serverData) {
+      // Create new server in database
+      await serverSchema.create({
+        serverID: server.id,
+        users: [],
+      });
+
+      serverData = await serverSchema.findOne({ serverID: server.id });
+    }
+
+    let userStats = await serverData.users.find(
+      (foundUser) => foundUser.userID == String(user.id)
+    );
 
     if (!userStats) {
-      await userSchema.create({
-        userID: String(user.id),
-        messagingLevel: 0,
-        reactingLevel: 0,
-        discussingLevel: 0,
-        hostingLevel: 0,
-        editingLevel: 0,
-        cleaningLevel: 0,
-        totalLevel: 0,
-      });
-      userStats = await userSchema.findOne({ userID: String(user.id) });
+      // Create new user in database
+      await serverSchema.updateOne(
+        { serverID: server.id },
+        {
+          $push: {
+            users: {
+              userID: user.id,
+              messagingLevel: 0,
+              reactingLevel: 0,
+              discussingLevel: 0,
+              hostingLevel: 0,
+              editingLevel: 0,
+              cleaningLevel: 0,
+              totalLevel: 0,
+              messagingCD: new Date(0),
+              reactingCD: new Date(0),
+              discussingCD: new Date(0),
+              hostingCD: new Date(0),
+              editingCD: new Date(0),
+              cleaningCD: new Date(0),
+            },
+          },
+        }
+      );
+
+      serverData = await serverSchema.findOne({ serverID: server.id });
+
+      userStats = await serverData.users.find(
+        (foundUser) => foundUser.userID == String(user.id)
+      );
     }
 
     const embed = new EmbedBuilder()
