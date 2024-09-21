12cd813848ce4331ad30bf0cf49c88928c2c68c9
Michael Connolly <mpc63@uakron.edu>
2024-09-20 23:26:51 -0400

    Add active events to server schema

--- schemas/server.js
+++ schemas/server.js
@@ -1,9 +1,11 @@
-const { model, Schema } = require('mongoose');
-const userSchema = require('./user');
+const { model, Schema } = require("mongoose");
+const userSchema = require("./user");
+const eventSchema = require("./event");
 
 const serverSchema = new Schema({
   serverID: String,
   users: [userSchema.schema],
+  events: [eventSchema.schema],
 });
 
-module.exports = model('servers', serverSchema);
+module.exports = model("servers", serverSchema);
